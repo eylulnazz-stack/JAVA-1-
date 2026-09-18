f5f7788f00acd7b1d52589762a8f9143fa58bfa1
eylulnazcamtepe <eylulnazz@kbu.ac.kr>
2026-09-18 14:13:04 +0900

    3차 수업.

---
+++ week03/sample03-1/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    boolean flag; // false;
+    byte value1 = -128; // -128 -127
+    short value2 = 32767;     // -32,768 ~ + 32,767
+    int value3 = 444444444;
+    long value4 = 4444444444446666L;
+    double value5 = 4.5555555555555555555555555;
+    float value6 = 4.5555555555555555555f;
+    char value7 = 'A';
+    String value9 = "AB";
+
+}
---
+++ week03/sample03-6/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    String name = "홍길동";
+    float height = 170.6f;
+    double weight = 67.5;
+    int age = 21;
+    char gender = '남';
+
+    System.out.printf("%s의 키는 %.1fCm입니다.\n", name, height);
+    System.out.printf("%s의 몸무게는  %.1fKg입니다.\n", name, weight);
+    System.out.printf("%s의 나이는 %d살입니다.\n", name, age);
+    System.out.printf("%s은 %c자입니다.\n", name, gender);
+
+}
